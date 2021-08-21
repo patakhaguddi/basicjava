1654d2f7ab2accb0cd59f54f950f962ee5b503e1
Saima Ahmed Rahin <rahinahmed8@gmail.com>
2021-08-21 16:10:31 +0600

    Extract HTML

--- src/main/java/WhileLoop/ExtractHTML.java
+++ src/main/java/WhileLoop/ExtractHTML.java
@@ -8,17 +8,21 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExtractHTML {
-    public static void main(String[] args) {
-        try {
-            String fileName = "sample.html";
-            File file = new File(fileName);
 
-            for (Element element : elements) {
-                String id = element.id();
-                System.out.println("ID: " + id);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+
+        String html="<html>\n" +
+                "<body>\n" +
+                "<div>\n" +
+                "<p>Transaction Successful</p>\n" +
+                "<p>Trnx ID: TXN5164870</p>\n" +
+                "</div>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>";
+
+        int pos=html.indexOf("TXN");
+        System.out.println(html.substring(pos,72));
+
     }
-            }
+}
